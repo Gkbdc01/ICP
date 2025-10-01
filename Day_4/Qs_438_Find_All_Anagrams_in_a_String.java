b278b9feb31818c9e29a41827f3b8d99ab96dd1d
Gaurav Kumar <rudraksha170@gmail.com>
2025-10-01 07:47:33 +0530

    list containing all the start idx from where it make a anagram of p

---
+++ Day_4/Qs_438_Find_All_Anagrams_in_a_String.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> arr =  new ArrayList<>();
+        int k = p.length();
+        for(int i = 0;i<=s.length()-k;i++){
+            if(isequal(p,s.substring(i,i+k))){
+                arr.add(i);
+            }
+        }
+        return arr;
+    }
+    public static boolean isequal(String a,String b){
+        int[] arr = new int[26];
+        for(int i = 0;i<a.length();i++){
+            arr[a.charAt(i)-'a'] += 1;
+            arr[b.charAt(i)-'a'] -= 1;
+        }
+        for(int i : arr){
+            if(i != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}

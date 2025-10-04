3db96d41587365b68b8c5f130cf1591b6d5fd0b7
Gaurav Kumar <rudraksha170@gmail.com>
2025-10-04 08:06:00 +0530

    return all combination whose sum equals to target

---
+++ Day_6/Qs_40_Combination_Sum_II.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> list = new LinkedList<List<Integer>>();
+        Arrays.sort(candidates);
+        isvalid(list, new ArrayList<Integer>(), candidates, target, 0);
+        return list;
+    }
+
+    private void isvalid(List<List<Integer>> answer,List<Integer> tempList,int[] candidates,int totalLeft,int index) {
+        if (totalLeft < 0) return;
+        else if (totalLeft == 0) {
+            answer.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = index;i < candidates.length && totalLeft >= candidates[i];i++) {
+                if (i > index && candidates[i] == candidates[i - 1]) continue;
+                tempList.add(candidates[i]);
+                isvalid(answer,tempList,candidates,totalLeft - candidates[i],i + 1);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}

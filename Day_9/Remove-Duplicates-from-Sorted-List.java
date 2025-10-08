3f9d9a7a4d63670ff38c4c9afed4be154a1a0eba
Gaurav Kumar <rudraksha170@gmail.com>
2025-10-08 06:25:58 +0530

    remove duplicats from the sorted Linked List

--- Day_9/Remove-Duplicates-from-Sorted-List.java
+++ Day_9/Remove-Duplicates-from-Sorted-List.java
@@ -1,19 +1,28 @@
-        ListNode node = new ListNode(1000,head);
-        ListNode ans = node.next;
-        while(head != null){
-            if(node.val != head.val){
-                node.next = head;
-                node = node.next;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
 
-    public ListNode deleteDuplicates(ListNode head) {
-class Solution {
-                head = head.next;
-            }else{
-                head = head.next;
-            }
-        }
-        node.next = null;
-        return ans;
-    }
+        ListNode node = new ListNode(1000,head);
+        ListNode ans = node.next;
+        while(head != null){
+            if(node.val != head.val){
+                node.next = head;
+                node = node.next;
+                head = head.next;
+            }else{
+                head = head.next;
+            }
+        }
+        node.next = null;
+        return ans;
+    }
 }
- */

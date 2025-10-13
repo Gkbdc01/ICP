9834dd3138f44ec38e8857d0daf39d22bb925749
Gkbdc01 <142567524+Gkbdc01@users.noreply.github.com>
2025-10-13 07:14:07 +0530

    feat: Solve LeetCode problem 'Make The String Great'

---
+++ Day_9/Make-The-String-Great.java
@@ -0,0 +1,15 @@
+        }
+                st.pop();
+            }else{
+            }else{
+            }
+                st.add(s.charAt(i));
+                st.add(s.charAt(i));
+            }
+        String res = "";
+        while(!st.isEmpty()){
+            res = st.pop()+res;
+        }
+        return res;
+    }
+}

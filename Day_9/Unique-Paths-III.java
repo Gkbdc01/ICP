ae54df2b05485041908c2f8ac10ef1da481a4c31
Gkbdc01 <142567524+Gkbdc01@users.noreply.github.com>
2025-10-13 07:14:09 +0530

    feat: Solve LeetCode problem 'Unique Paths III'

--- Day_9/Unique-Paths-III.java
+++ Day_9/Unique-Paths-III.java
@@ -1,20 +1,19 @@
-            return 0;
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == -1)
-    public int dfs(int[][] grid, int i, int j, int empty) {
-    }
-
-        return dfs(grid, startX, startY, empty);
-            }
         }
-        
-                }
-                else if (grid[i][j] == 1) {
-                    startX = i;
-                    startY = j;
-                if (grid[i][j] == 0) empty++;
-            for (int j = 0; j < grid[0].length; j++) {
-        for (int i = 0; i < grid.length; i++) {
-        
-        int empty = 0, startX = 0, startY = 0;
-    public int uniquePathsIII(int[][] grid) {
-class Solution {
+
+        int temp = grid[i][j];
+        grid[i][j] = -1; // mark visited
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+            return;
+            if (empty == 0) result++;
+        if (grid[i][j] == 2) {
+        for (int d = 0; d < 4; d++) {
+            dfs(grid, i + dx[d], j + dy[d], temp == 0 ? empty - 1 : empty);
+        // Reached the end
+
+            return;
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == -1)
+        // Out of bounds or obstacle
+    private void dfs(int[][] grid, int i, int j, int empty) {

49a618e9275c66a789db9be0a834eb33c6763286
Gaurav Kumar <rudraksha170@gmail.com>
2025-10-21 19:37:19 +0530

    implement a stack using queue

---
+++ Day_10/Qs_225_Implement_Stack_using_Queues.java
@@ -0,0 +1,35 @@
+class MyStack {
+
+    private List<Integer> q ;
+    public MyStack() {
+        q = new ArrayList<>();
+    }
+    
+    public void push(int x) {
+        q.add(x);
+    }
+    
+    public int pop() {
+        return q.remove(q.size()-1);
+    }
+    
+    public int top() {
+        return q.get(q.size()-1);
+    }
+    
+    public boolean empty() {
+        if(q.size() == 0){
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
